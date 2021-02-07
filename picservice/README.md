0ed8e284429a586b6fdaedc00fe7b58cba37fed8
Michael Klein <dev@michael-klein.info>
2021-02-07 21:04:31 +0100

    initial picservice v0.1.0

--- picservice/README.md
+++ picservice/README.md
@@ -1,6 +1,53 @@
 # picservice
 
-## sources
+A simple API server that draws predefined pictures on the Pi's Sense HAT.
 
-- https://magpi.raspberrypi.org/articles/learn-sense-hat-with-raspberry-pi
-- https://github.com/nathany/bobblehat
+## Prerequisites
+
+- Sense HAT set up (out of scope here)
+- Dependencies installed:
+
+```bash
+$ make get
+go get github.com/gin-gonic/gin
+go get github.com/nathany/bobblehat/sense/screen
+go get github.com/nathany/bobblehat/sense/screen/texture
+go get github.com/sirupsen/logrus
+```
+
+## Build
+
+```bash
+$ make build
+go build -o picservice
+```
+
+## Run
+
+### Non-Production
+
+```bash
+$ ./picservice
+...
+[GIN-debug] [WARNING] Running in "debug" mode. ...
+[GIN-debug] GET    /api/v1/screen/clear      --> main.ScreenClearHandler (4 handlers)
+[GIN-debug] GET    /api/v1/screen/draw/:pic  --> main.ScreenDrawHandler (4 handlers)
+INFO[0000] picservice start...                          
+[GIN-debug] Environment variable PORT is undefined. Using port :8080 by default
+[GIN-debug] Listening and serving HTTP on :8080
+```
+
+### Production
+
+```bash
+$ GIN_MODE=release ./picservice 
+frame buffer device not found
+INFO[0000] picservice start...
+```
+
+## Sources
+
+- [Pixel art on Sense HAT](https://magpi.raspberrypi.org/articles/pixel-art-on-sense-hat)
+- [Learn Sense HAT with Raspberry Pi](https://magpi.raspberrypi.org/articles/learn-sense-hat-with-raspberry-pi)
+- [bobbleHAT](https://github.com/nathany/bobblehat)
+- [Gin Web Framework](https://github.com/gin-gonic/gin)
--- picservice/main.go
+++ picservice/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	_ "image/png"
 	"net/http"
 	"os"
@@ -12,42 +13,38 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// ScreenClearHandler TODO
+// ScreenClearHandler clears the display of the Pi's Sense HAT
 func ScreenClearHandler(c *gin.Context) {
 	screen.Clear()
 
 	c.JSON(http.StatusOK, gin.H{"message": "cleared", "status": http.StatusOK})
 }
 
-// ScreenDrawHandler TODO
+// ScreenDrawHandler draws the given pic on the Pi's Sense HAT
 func ScreenDrawHandler(c *gin.Context) {
+	pic := c.Param("pic")
+	if pic == "" {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "malformed requerst", "status": http.StatusBadRequest})
+	}
 
 	wd, _ := os.Getwd()
-	pic := wd + "/pics/key.png"
+	pic = fmt.Sprintf("%s/pics/%s.png", wd, pic) // pictures come from pics subfolder, PNGs only
 
 	tx, err := texture.Load(pic)
 	if err != nil {
 		log.Error(err)
 		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"message": "requested resource not found", "status": http.StatusNotFound})
 	}
+
+	// draw PNG, first 8x8, starting at 0,0
 	fb := screen.NewFrameBuffer()
-	texture.Blit(fb.Texture, 0, 0, tx, 0, 0, 8, 8) // 8x8, starting at 0,0
+	texture.Blit(fb.Texture, 0, 0, tx, 0, 0, 8, 8)
 	screen.Draw(fb)
 
 	c.JSON(http.StatusOK, gin.H{"message": "drawn", "status": http.StatusOK})
 }
 
-// LivenessHandler TODO
-func LivenessHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "alive", "status": http.StatusOK})
-}
-
-// ReadinessHandler TODO
-func ReadinessHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"message": "ready", "status": http.StatusOK})
-}
-
-// NotFoundHandler to indicate that requested resource could not be found
+// NotFoundHandler is to indicate that requested resource could not be found
 func NotFoundHandler(c *gin.Context) {
 	// log this event as it could be an attempt to break in...
 	log.Infoln("Not found, requested URL path:", c.Request.URL.Path)
@@ -56,22 +53,16 @@ func NotFoundHandler(c *gin.Context) {
 
 // SetupRouter is published here to allow setup of tests
 func SetupRouter() *gin.Engine {
-
 	router := gin.Default()
 
 	router.Use(gin.Recovery()) // "recover from any panics", write 500 if any
 
 	router.NoRoute(NotFoundHandler)
 
-	// public, generic API
-	router.GET("/healthy", LivenessHandler)
-	router.GET("/ready", ReadinessHandler)
-
-	// Simple group: v1
-	v1 := router.Group("/v1")
+	v1 := router.Group("/api/v1/screen")
 	{
-		v1.GET("/screen/clear", ScreenClearHandler)
-		v1.GET("/screen/draw", ScreenDrawHandler)
+		v1.GET("/clear", ScreenClearHandler)
+		v1.GET("/draw/:pic", ScreenDrawHandler)
 	}
 
 	return router
@@ -80,8 +71,8 @@ func SetupRouter() *gin.Engine {
 func main() {
 	router := SetupRouter()
 
-	log.Infoln("web app start...")
-	defer log.Infoln("web app shutdown!")
+	log.Infoln("picservice start...")
+	defer log.Infoln("picservice shutdown!")
 
 	// set port via PORT environment variable
 	router.Run() // default port is 8080
